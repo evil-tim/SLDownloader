2d888b7169cba6ca01626427a33fde9c9dcdc0d6
evil_tim <evil_tim@email>
2017-02-26 11:45:37 +0800

    setup entity

--- src/main/java/com/sldlt/navps/entity/NAVPSEntry.java
+++ src/main/java/com/sldlt/navps/entity/NAVPSEntry.java
@@ -1,12 +1,30 @@
 package com.sldlt.navps.entity;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class NAVPSEntry {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class NAVPSEntry implements Serializable {
+
+    private static final long serialVersionUID = -3464455970423863740L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
     private String fund;
+
+    @Column(nullable = false)
     private LocalDate date;
+
+    @Column(nullable = false)
     private BigDecimal value;
 
     public String getFund() {
