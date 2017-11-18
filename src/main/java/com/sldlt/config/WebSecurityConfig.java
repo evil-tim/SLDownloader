16c52e36541931dc7495b10619cdd6063b5c230f
evil_tim <epsilon_recursor@yahoo.com>
2017-11-18 11:22:10 +0800

    add cache header to static content and webjars

--- src/main/java/com/sldlt/config/WebSecurityConfig.java
+++ src/main/java/com/sldlt/config/WebSecurityConfig.java
@@ -9,7 +9,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http
+            .authorizeRequests()
+                .antMatchers("/api").permitAll()
+                .antMatchers("/**").permitAll();
     }
 
 }
