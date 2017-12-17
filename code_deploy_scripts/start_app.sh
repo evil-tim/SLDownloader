f375bbf7abcb058970123410b9f2e337c58220b3
evil_tim <epsilon_recursor@yahoo.com>
2017-12-17 14:12:31 +0800

    update paths and pemissions

--- code_deploy_scripts/start_app.sh
+++ code_deploy_scripts/start_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd /home/ubuntu/sldownloader-app
 /usr/bin/docker image build --build-arg JAR_FILE=sldownloader.jar .
 /usr/local/bin/docker-compose up -d
--- code_deploy_scripts/stop_app.sh
+++ code_deploy_scripts/stop_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd /home/ubuntu/sldownloader-app
 /usr/local/bin/docker-compose down
