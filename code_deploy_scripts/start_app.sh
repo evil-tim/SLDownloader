21fa7f343ab0a304c4e5ad30edee4a7e81d06336
evil_tim <epsilon_recursor@yahoo.com>
2017-12-17 15:13:48 +0800

    update image build command

--- code_deploy_scripts/start_app.sh
+++ code_deploy_scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu/sldownloader-app
-/usr/bin/docker image build --build-arg JAR_FILE=sldownloader.jar .
+/usr/bin/docker image build --build-arg JAR_FILE=sldownloader.jar -t crabranch/sldownloader:0.0.3-SNAPSHOT .
 /usr/local/bin/docker-compose up -d
