642eb6903d046652ce65fce2b7562b9fc1a95d97
evil_tim <epsilon_recursor@yahoo.com>
2018-06-02 11:38:44 +0800

    implement bollinger band indicator

---
+++ src/main/resources/static/js/analysis/bollinger_band.js
@@ -0,0 +1,65 @@
+(function() {
+
+    function init(settings) {
+
+    }
+
+    function columns(settings) {
+        return [ {
+            'type' : 'number',
+            'title' : '' + settings.period + 'D BB',
+            'color' : settings.color1
+        }, {
+            'type' : 'number',
+            'title' : '' + settings.period + 'D BB Upper',
+            'color' : settings.color2
+        }, {
+            'type' : 'number',
+            'title' : '' + settings.period + 'D BB Lower',
+            'color' : settings.color2
+        } ];
+    }
+
+    function compute(index, data, settings) {
+        var accum = 0;
+        var stdDevAccum = 0;
+        var valid = true;
+        for (var i = 0; valid && i < settings.period; i++) {
+            var dataIndex = index + i;
+            if (dataIndex < data.length) {
+                accum += data[dataIndex];
+            } else {
+                valid = false;
+            }
+        }
+        if (valid) {
+            var average = accum / settings.period;
+
+            for (var i = 0; valid && i < settings.period; i++) {
+                var dataIndex = index + i;
+                stdDevAccum += (data[dataIndex] - average)
+                        * (data[dataIndex] - average);
+            }
+
+            var stdDev = Math.sqrt(stdDevAccum / (settings.period - 1));
+            return [ average, average + stdDev * 2, average - stdDev * 2 ];
+        } else {
+            return [ null, null, null ];
+        }
+    }
+
+    var bb20Settings = {
+        'period' : 20,
+        'color1' : '#d81b60',
+        'color2' : '#f06292'
+    };
+    var bb160Settings = {
+        'period' : 160,
+        'color1' : '#8e24aa',
+        'color2' : '#ba68c8'
+    };
+
+    analysisModules.addModule('bb_20', columns, init, compute, bb20Settings);
+    analysisModules.addModule('bb_160', columns, init, compute, bb160Settings);
+
+}());
