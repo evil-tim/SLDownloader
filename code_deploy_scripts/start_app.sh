dff0135c09e397abfeef1b5090103e51273ada2f
evil_tim <epsilon_recursor@yahoo.com>
2019-05-26 14:33:56 +0800

    change build to use AWS ECR

--- code_deploy_scripts/start_app.sh
+++ code_deploy_scripts/start_app.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /home/ubuntu/sldownloader-app
-/usr/bin/docker image build --build-arg JAR_FILE=sldownloader.jar -t crabranch/sldownloader:0.0.6-SNAPSHOT .
 /usr/local/bin/docker-compose up -d
