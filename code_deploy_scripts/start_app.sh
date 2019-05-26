248f82d8fec52e4b0c75ffc6292079c9db15650f
evil_tim <epsilon_recursor@yahoo.com>
2019-05-26 19:36:50 +0800

    fix ECR credentials

--- code_deploy_scripts/start_app.sh
+++ code_deploy_scripts/start_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/sldownloader-app
-$(aws ecr get-login)
+aws configure set region `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//'`
+$(aws ecr get-login --no-include-email)
 /usr/local/bin/docker-compose up -d
