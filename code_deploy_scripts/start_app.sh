c0d5c777bfb8b66042bbb4d96e5a4a71567050e5
evil_tim <epsilon_recursor@yahoo.com>
2019-06-09 15:03:32 +0800

    force download of latest version of application image

--- code_deploy_scripts/start_app.sh
+++ code_deploy_scripts/start_app.sh
@@ -3,4 +3,6 @@
 cd /home/ubuntu/sldownloader-app
 aws configure set region `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//'`
 $(aws ecr get-login --no-include-email)
+docker pull 535425158818.dkr.ecr.ap-southeast-1.amazonaws.com/crabranch/sldownloader:latest
+docker rmi $(docker images -f "dangling=true" -q)
 /usr/local/bin/docker-compose up -d
