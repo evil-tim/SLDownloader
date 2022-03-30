30d2744edb48feebe5b5fa9b4f10763b7bb4ed02
James Villegas <evil-tim@users.noreply.github.com>
2022-03-30 15:25:12 +0800

    exclude static resources from spring filter

--- src/main/java/com/sldlt/config/WebSecurityConfig.java
+++ src/main/java/com/sldlt/config/WebSecurityConfig.java
@@ -1,12 +1,21 @@
 package com.sldlt.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter.ReferrerPolicy;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+                .antMatchers("/js/**")
+                .antMatchers("/css/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
