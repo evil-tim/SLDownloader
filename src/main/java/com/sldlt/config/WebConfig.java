4fd8b54083efaf0bd37b1c34189af5eba87da4a0
evil-tim <10543339+evil-tim@users.noreply.github.com>
2026-05-03 17:56:42 +0800

    cors allow all

--- src/main/java/com/sldlt/config/WebConfig.java
+++ src/main/java/com/sldlt/config/WebConfig.java
@@ -4,9 +4,17 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**") //
+                .allowedOrigins("*") //
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //
+                .allowedHeaders("*");
+    }
 }
